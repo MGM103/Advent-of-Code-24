633618d1dd4851577c91117a77b5b16f7353c5c4
MGM103 <marcus.marinelli3@gmail.com>
2024-12-03 21:07:35 +1100

    Adding solution for day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -18,8 +19,16 @@ func main() {
 
 	validData := ExtractValidData(file)
 	multiplicationsOutput := CalcMultiplications(validData)
-
 	println("Multiplications result: ", multiplicationsOutput)
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to reset file position: %v", err))
+	}
+
+	validDataWithConditionals := ExtractValidDataWithConditionals(file)
+	multiplicationsOutputWithConditionals := CalcMultiplications(validDataWithConditionals)
+	println("Multiplications result w/ conditionals: ", multiplicationsOutputWithConditionals)
 }
 
 func ExtractValidData(corruptedData io.Reader) [][]int {
@@ -48,6 +57,49 @@ func ExtractValidData(corruptedData io.Reader) [][]int {
 			}
 
 			validMultiplications = append(validMultiplications, []int{x, y})
+		}
+	}
+
+	return validMultiplications
+}
+
+func ExtractValidDataWithConditionals(corruptedData io.Reader) [][]int {
+	regexPattern := `mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)`
+	regex := regexp.MustCompile(regexPattern)
+
+	validMultiplications := make([][]int, 0)
+	scanner := bufio.NewScanner(corruptedData)
+
+	instructionsEnabled := true
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		matches := regex.FindAllSubmatch([]byte(line), -1)
+
+		for _, match := range matches {
+			if instructionsEnabled && strings.Contains(string(match[0]), "mul") {
+				x, errX := strconv.Atoi(string(match[1]))
+				if errX != nil {
+					fmt.Printf("Could not convert %s into int: %v\n", match[1], errX)
+					continue
+				}
+
+				y, errY := strconv.Atoi(string(match[2]))
+				if errY != nil {
+					fmt.Printf("Could not convert %s into int: %v\n", match[2], errY)
+					continue
+				}
+
+				validMultiplications = append(validMultiplications, []int{x, y})
+			} else {
+				instruction := string(match[0])
+
+				if instruction == "don't()" {
+					instructionsEnabled = false
+				} else if instruction == "do()" {
+					instructionsEnabled = true
+				}
+			}
 		}
 	}
 
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -22,6 +22,21 @@ func TestExtractValidData(t *testing.T) {
 	})
 }
 
+func TestExtractValidDataWithConditionals(t *testing.T) {
+	data := `where()(< }when()mul(678,62)%mul(747,584)from()-   select(59,725)mul(570,425)<~^:$where()!@}where()mul(542,816):don't()select()from()#(;!select()mul(541,668)mul(557,427)what()<#-why(952,537)*what()}mul(723,604)/^]'+;from()('-mul(227,146)'what()~*who():select()~from()do()mul(640,593)
+	xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
+	mockData := strings.NewReader(data)
+
+	t.Run("extracts valid mul calls from data", func(t *testing.T) {
+		got := day3.ExtractValidDataWithConditionals(mockData)
+		want := [][]int{{678, 62}, {747, 584}, {570, 425}, {542, 816}, {640, 593}, {2, 4}, {8, 5}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v, instead got %v", want, got)
+		}
+	})
+}
+
 func TestCalcMultiplications(t *testing.T) {
 	cases := []struct {
 		name  string
@@ -31,6 +46,7 @@ func TestCalcMultiplications(t *testing.T) {
 		{"Empty", make([][]int, 0), 0},
 		{"Single", [][]int{{3, 2}}, 6},
 		{"Multiple entries", [][]int{{2, 4}, {5, 5}, {11, 8}, {8, 5}}, 161},
+		{"With conditionals", [][]int{{2, 4}, {8, 5}}, 48},
 	}
 
 	for _, test := range cases {
